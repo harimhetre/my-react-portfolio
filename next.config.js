c44555a26c67aff82077af77caa01b1d93ae5905
Hari Mhetre <42270148+harimhetre@users.noreply.github.com>
2022-01-12 19:03:07 +0530

    gh-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
 	reactStrictMode: false,
 	images: {
 		domains: ["avatars.githubusercontent.com"],
-	}
+	},
+	assetPrefix: isProd ? '/your-github-repo-name/' : ''
 };
