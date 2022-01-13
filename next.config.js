f7895432bbc0a3e8ce922405abc547a606879ac9
harimhetre <harimhetre@users.noreply.github.com>
2022-01-13 09:18:09 +0000

    deploy: 5661949a882d946e24a0ab6ed0fc9e3d74db5043

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-	reactStrictMode: false,
-	images: {
-		domains: ["avatars.githubusercontent.com"],
-	},
-	assetPrefix: isProd ? '/your-github-repo-name/' : ''
-};
